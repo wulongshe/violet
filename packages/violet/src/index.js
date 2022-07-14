e563d863ff51c1df647aae86219967ed9263b65d
Yuki-0505 <2243104010@qq.com>
2022-07-14 20:43:57 +0800

    fix: 删除创建模板功能

--- packages/violet/src/index.js
+++ packages/violet/src/index.js
@@ -1,5 +1,4 @@
 import { cac } from 'cac'
-import { create } from '@violet-plus/create'
 import { server } from './server.js'
 import { useConfig, mergeConfig } from './config.js'
 
@@ -15,14 +14,6 @@ cli
   .action(async (options) => {
     mergeConfig(options)
     server(root, { ...config, ...options })
-  })
-
-// create
-cli
-  .command('create [projectName]')
-  .action(async (projectName, options) => {
-    console.log('[projectName] ', projectName)
-    create({ root, projectName, ...options })
   })
 
 cli.help()
